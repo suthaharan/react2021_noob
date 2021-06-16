1f64a6c84c6880124c51065e10c35dd112c39388
sutha <sutha@kurinchilion.com>
2021-06-16 15:53:42 -0400

    import and add images as avatar to user display

--- contactlistapp/src/components/ContactCard.js
+++ contactlistapp/src/components/ContactCard.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import usericon from '../images/panda.png';
 
 const ContactCard = (props) => {
     // destructure the props
     const {id, name, email} = props.contact;
     return (
         <tr>
-        <td data-label="Name">{name}</td>
+        <td data-label="Name"><img className="ui avatar image" src={usericon} alt="user" />{name}</td>
         <td data-label="Email">{email}</td>
         <td data-label="Action"><i className="trash alternate outline icon" style={{color: "red", marginTop:"2px"}}></i></td>
         </tr>
